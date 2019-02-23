8e6897ee972ba659c4def34511417cf4c22a5ab4
zenthangplus <zenthangplus@gmail.com>
2019-02-23 18:26:23 +0700

    Init Golang

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/trend-ai/TrendAI_mobile_backend/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
