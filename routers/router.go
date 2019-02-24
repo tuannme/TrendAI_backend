05debe4fb1ad57f198ba291679386bea52087264
zenthangplus <zenthangplus@gmail.com>
2019-02-25 00:16:44 +0700

    Use bee run in development

--- routers/router.go
+++ routers/router.go
@@ -1,7 +1,7 @@
 // @APIVersion 1.0.0
-// @Title beego Test API
-// @Description beego has a very cool tools to autogenerate documents for your API
-// @Contact astaxie@gmail.com
+// @Title TrendAI Backend APIs
+// @Description APIs document for TrendAI Backend app
+// @Contact zenthangplus@gmail.com
 // @TermsOfServiceUrl http://beego.me/
 // @License Apache 2.0
 // @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
