ac8d801933a0e333ae9fae05c31e1cbe21fcc393
zenthangplus <zenthangplus@gmail.com>
2019-02-28 22:51:24 +0700

    Load app conf in env file

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	_ "github.com/trend-ai/TrendAI_mobile_backend/conf"
 	_ "github.com/trend-ai/TrendAI_mobile_backend/routers"
 	"github.com/trend-ai/TrendAI_mobile_backend/services/databases"
 )
 
 func main() {
+	logs.Debug("App initiated: ", beego.BConfig.AppName)
+	logs.Debug("App run mode: ", beego.BConfig.RunMode)
+
 	// Get firestore client
 	firestoreClient := databases.GetFirestoreClient()
 
